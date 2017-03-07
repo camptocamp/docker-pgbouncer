5d3b87bafe39a13af75c254c076ffaff4addf1fb
Raphaël Pinson <raphael.pinson@camptocamp.com>
2017-03-07 16:14:21 +0100

    Add PG_HBA and USERLIST to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,13 @@ mkdir -p ${PG_LOG}
 chmod -R 755 ${PG_LOG}
 chown -R ${PG_USER}:${PG_USER} ${PG_LOG}
 
+if test -n "${PG_HBA}"; then
+  echo "${PG_HBA}" > /etc/pgbouncer/pg_hba.conf
+fi
+
+if test -n "${USERLIST}"; then
+  echo "${USERLIST}" > /etc/pgbouncer/userlist.txt
+fi
+
 echo "Starting pgbouncer..."
 exec pgbouncer -vvvv -u ${PG_USER} $PG_CONFIG
